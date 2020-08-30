a1c6fe5a81af0a17724bdbf0715b6030228f5d77
EAdemov <e.ademov@casualino.com>
2020-08-30 16:27:49 +0300

    Updated discord4j and added .env library.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will be printed");
+        final String token = "";
+        final DiscordClient client = DiscordClient.create(token);
+        final GatewayDiscordClient gateway = client.login().block();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage("Pong!").block();
+            }
+        });
+
+        gateway.onDisconnect().block();
     }
 }
